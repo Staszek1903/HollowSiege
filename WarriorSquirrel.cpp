7b91463ea5b47ac46e28b6e746d55eaf6e5b33f1
Meghtar <maciej.pypec@gmail.com>
2018-02-25 19:42:23 +0100

    worm update

--- WarriorSquirrel.cpp
+++ WarriorSquirrel.cpp
@@ -12,8 +12,27 @@ WarriorSquirrel::~WarriorSquirrel()
 {
 }
 
+float WarriorSquirrel::Distance(sf::Vector2f a, sf::Vector2f b)
+{
+	return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+}
+
 void WarriorSquirrel::Attack(Entity & character)
 {
 	character.HP -= damagePoints;
 	cooldown = cooldownConst;
+}
+
+sf::Vector2f WarriorSquirrel::TargetClosestEnemyPosition(std::vector<Worm> worms)
+{
+	float minDist = Distance(worms.at(0).sprite.getPosition,);
+	for (int i = 1; i < worms.size(); ++i)
+	{
+
+	}
+	return sf::Vector2f();
+}
+
+void WarriorSquirrel::MoveToPosition(sf::Vector2f position)
+{
 }
