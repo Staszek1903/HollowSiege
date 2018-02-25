ffb53eaf3842c1e549e7f205b90bff41f638075e
Meghtar <maciej.pypec@gmail.com>
2018-02-25 20:05:25 +0100

    warrior squirrel update

--- WarriorSquirrel.cpp
+++ WarriorSquirrel.cpp
@@ -19,20 +19,39 @@ float WarriorSquirrel::Distance(sf::Vector2f a, sf::Vector2f b)
 
 void WarriorSquirrel::Attack(Entity & character)
 {
-	character.HP -= damagePoints;
-	cooldown = cooldownConst;
+	if (cooldown == 0)
+	{
+		character.HP -= damagePoints;
+		cooldown = cooldownConst;
+	}
 }
 
 sf::Vector2f WarriorSquirrel::TargetClosestEnemyPosition(std::vector<Worm> worms)
 {
-	float minDist = Distance(worms.at(0).sprite.getPosition,);
-	for (int i = 1; i < worms.size(); ++i)
+	std::pair<int, float> minDist;
+	minDist.first = 0;
+	minDist.second = Distance(worms.at(0).sprite.getPosition(),sprite.getPosition());
+	float tmpDist;
+	for (unsigned i = 1; i < worms.size(); ++i)
 	{
-
+		tmpDist = Distance(worms.at(i).sprite.getPosition(), sprite.getPosition());
+		if (tmpDist<minDist.second && tmpDist<=range)
+		{
+			minDist.first = i;
+			minDist.second = tmpDist;
+		}
 	}
-	return sf::Vector2f();
+	return worms.at(minDist.first).sprite.getPosition();
 }
 
 void WarriorSquirrel::MoveToPosition(sf::Vector2f position)
 {
+	if (position.x > sprite.getPosition().x)
+		move({ speed, 0 });
+	else
+		move({ -speed,0 });
+	if (position.y > sprite.getPosition().y)
+		move({ 0,speed });
+	else 
+		move({ 0,-speed });
 }
